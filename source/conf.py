b50fb8df58369d0b50ca5985d37e7dddac15d43a
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-25 15:27:58 -0400

    initial checkin

---
+++ source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Galactic Dynamics School Handbook'
+copyright = '2021, the SOC'
+author = 'the SOC'
+
+
+# -- General configuration ---------------------------------------------------
+
+# By default, highlight as Python 3.
+highlight_language = 'python3'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = []
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,7 @@
+
+Welcome to the Applied Galactic Dynamics School Handbook
+========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
